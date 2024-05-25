4ffb9daa5e8790a484dc2a10ffa6d7d82d4b71c6
doniyor <doniyorjumaniyazov5@gmail.com>
2024-05-25 21:47:18 +0500

    added

---
+++ accoroding_book.py
@@ -0,0 +1,55 @@
+# str  = input("hello world!,'this is not main rule i it'") # "" '' gjkpetncz lkz cnhjrb yj "" ,jktt njxtxtysq
+# print(str.title())
+
+# name = "MY Angel"
+# print(name.title()) # сделает болшым первый букв каждый слов
+# print(name.upper()) # переобразует все буков болши
+# print(name.lower()) # наоборот
+
+# first_name = "Doniyor"
+# sure_name = "Jumaniyazov"
+# full_name = first_name + " " + sure_name  # + обединает строки канкатация
+# print(full_name)
+# print("Hello " + full_name.upper() + " ! ")
+# print("Hello " + full_name.lower() + " ! ")
+
+# print("cashback")
+# print("\tcashback \n \tbar \ncafe")   # \t типа пробел \т делает из строчный столбецев
+# fav_language = " c+ ! "
+# print(fav_language)
+# print(fav_language.rstrip())  # rstrip() удаляет конечный отступи 
+# print(fav_language.lstrip())  # наоборот
+# print(2 + 3)
+# print(6 * 5)
+# print(40 / 15)
+# print(0.2 + 0.3)
+# print(2 * 0.3)      
+   
+#import this    
+# список
+# list_1 = [2, 3, 4, 5, 6]
+# list_2 = []
+
+# for i in list_1:
+#     if i % 2 == 0:
+#         list_2.append(i)  # чтробы создат или пополнит внутри новый список при условия вот так делается     
+    
+# print(list_2)
+# names = ['asa', '22', 'dda', 'a1a1a', 'xzx']    
+# lists = "my build " + names[2] + " hello"
+# print(names[2], names[0])  # можно с помошю номера индекса показат значение
+# print(names[4].upper())
+# print(names[-2]) # показивает перед последный элемент
+
+
+# print(lists)
+cars = ['bmw', 'malibu', 'tico']
+print(cars)
+cars[0] = 'damas' # изменение значение списка местами индекса
+print(cars)
+cars[1] = 'audi'
+print(cars)
+cars.append('hongi') # добавление новый значение
+print(cars)
+cars.insert(0, 'byd') # это означает добавление новые значение указанные индекса при этом не удалят и не потеряв сушуствуюший значение
+print(cars) 
